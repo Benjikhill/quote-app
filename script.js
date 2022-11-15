0510dbaee77b492ee8780135b1ca8e18f4fc0ab4
Benjamin Hill <benjamin.hill@gmx.net>
2022-11-15 13:29:41 +0100

    fix: quote app is done

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ function quoteGarden() {
   fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
     .then((response) => response.json())
     .then((data) => {
-      document.body.append(document.createTextNode(data.random));
+      document.body.append(data.data[0].quoteText, data.data[0].quoteAuthor);
     });
 }
 
 const button = document.querySelector("button");
 button.addEventListener("click", quoteGarden);
-//button.style.backgroundColor = "red";
